cc7ddc49f8c0ffeb55afdc10bd2f3c18594e4ec8
sato-ua <69755969+sato-ua@users.noreply.github.com>
2025-11-15 15:23:01 +0200

    Update ecoflow_hardcodded.py

--- ecoflow_hardcodded.py
+++ ecoflow_hardcodded.py
@@ -3,10 +3,10 @@ from urllib.parse import quote_plus
 
 # ← ВСТАВ СВОЇ ДАНІ СЮДИ (копіюй прямо з developer.ecoflow.com і телеграм-бота)
 ACCESS_KEY = "9gZHSt6akN4bnsSWyPdOYHNsDfftXwkD"          # ← твій
-SECRET_KEY = "uB2lV7vQbH9kR1iT6yW8zX4cD0fG3hJ9mN5pQ7sU"   # ← твій
+SECRET_KEY = "iD1BP5w76HLbvV2erbq8CNWTK6MBp4HP"   # ← твій
 SERIAL = "R351ZEB4HG490907"                            # ← твій
-TELEGRAM_TOKEN = "7772486825:AAHNp7k..."               # ← твій
-CHAT_ID = 5123456789                                    # ← твій (число)
+TELEGRAM_TOKEN = "8387721988:AAEKwg4Gj0-7pBD8JMjYkMU1GwCEAAkAzEk"               # ← твій
+CHAT_ID = 317004830                                    # ← твій (число)
 
 URL = "https://api.ecoflow.com/iot-open/sign/device/quota"
 last = None
